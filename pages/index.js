7b2f4fd9cc7edc10cdd4488ab67468dcce91cb78
Mehdi Bounya <mehdi.bounya@gmail.com>
2020-06-02 17:40:04 +0100

    Style the home page

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,32 @@
 import Head from 'next/head'
+import {useState} from "react";
 
 export default function Home() {
+  const [secret, setSecret] = useState('');
+  const [payload, setPayload] = useState('');
+
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Paseto Debugger</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Paseto Debugger
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          An online debugger to decode Paseto tokens
         </p>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <form>
+              <input type="text" placeholder="Secret/Public key"/>
+              <input type="text" placeholder="Paseto token"/>
+              <textarea readOnly="true" rows="10" value={payload} />
+          </form>
         </div>
       </main>
 
@@ -187,6 +169,14 @@ export default function Home() {
             width: 100%;
             flex-direction: column;
           }
+        }
+        input {
+            height: 5vh;
+            margin-bottom: 2vh;
+        }
+        input, textarea {
+            width: 100vh;
+            border: #ccc solid 1px;
         }
       `}</style>
 
