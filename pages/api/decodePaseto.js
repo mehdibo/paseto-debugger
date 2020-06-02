98f4b726002e612c8d80dee0f67a5fe451283178
Mehdi Bounya <mehdi.bounya@gmail.com>
2020-06-02 18:17:55 +0100

    Decode paseto in decodePaseto.js

--- pages/api/decodePaseto.js
+++ pages/api/decodePaseto.js
@@ -1,6 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const Paseto = require('paseto.js');
+
+function getVersion(token) {
+  return token.split
+}
 
 export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+    const version = req.body.token.split(".")[0]
+    const pVersion = (version === 'v1') ? new Paseto.V1() : new Paseto.V2();
+    const sk = new Paseto.SymmetricKey(pVersion);
+    sk.hex(req.body.secret);
+
+    pVersion
+      .decrypt(req.body.token, sk)
+      .then(
+          function (m) {
+              res.statusCode = 200
+              res.json(JSON.parse(m))
+          }
+      );
 }
