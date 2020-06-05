bdd3f154ec876722a1ea31bdd254e05fbff80feb
Mehdi Bounya <mehdi.bounya@gmail.com>
2020-06-05 13:42:02 +0100

    Organise index.js structure

--- pages/index.js
+++ pages/index.js
@@ -44,43 +44,60 @@ export default function Home() {
               <title>Paseto Debugger</title>
               <link rel="icon" href="/favicon.ico" />
           </Head>
-          <Row>
-              <Col>
-                  <h1>
-                      Paseto Debugger
-                  </h1>
-                  <p className={"lead"}>
-                      An online debugger to decode Paseto tokens
-                  </p>
-                  <Alert variant="warning" className={"mx-auto"}>
-                      The secret you enter here is sent to a backend server to decode your Paseto token, but it is never saved.
-                      You can check the <a href={"https://github.com/mehdibo/paseto-debugger/"} className={"alert-link"}>source code here</a>
-                  </Alert>
-              </Col>
-          </Row>
-          <Row>
-              <Col className={"mx-auto"}>
-                  {error && (<Alert variant={"danger"}>{error}</Alert>)}
-                  <Form onSubmit={decodePaseto}>
-                      <Form.Group controlId="secretKey">
-                          <Form.Control type="text" placeholder="Secret in HEX format" onChange={(e) => setSecret(e.target.value)} />
-                          <Form.Text className="text-muted">
-                              The secret is sent to a backend API, but it is not saved
-                          </Form.Text>
-                      </Form.Group>
+          <header>
+              <Row>
+                  <Col>
+                      <h1>
+                          Paseto Debugger
+                      </h1>
+                      <p className={"lead"}>
+                          An online debugger to decode Paseto tokens
+                      </p>
+                      <Alert variant="warning" className={"mx-auto"}>
+                          The secret you enter here is sent to a backend server to decode your Paseto token, but it is never saved.
+                          You can check the <a href={"https://github.com/mehdibo/paseto-debugger/"} className={"alert-link"}>source code here</a>
+                      </Alert>
+                  </Col>
+              </Row>
+          </header>
+          <main className={"flex-shrink-0"}>
+              <Row>
+                  <Col className={"mx-auto"}>
+                      {error && (<Alert variant={"danger"}>{error}</Alert>)}
+                      <Form onSubmit={decodePaseto}>
+                          <Form.Group controlId="secretKey">
+                              <Form.Control type="text" placeholder="Secret in HEX format" onChange={(e) => setSecret(e.target.value)} />
+                              <Form.Text className="text-muted">
+                                  The secret is sent to a backend API, but it is not saved
+                              </Form.Text>
+                          </Form.Group>
 
-                      <Form.Group controlId="pasetoToken">
-                          <Form.Control type="text" placeholder="Paseto token" onChange={(e) => setToken(e.target.value)} />
-                      </Form.Group>
-                      <Button variant="primary" type="submit">
-                          Decode
-                      </Button>
-                  </Form>
-                  <div>
-                      <ReactJsonSyntaxHighlighter obj={payload} />
-                  </div>
-              </Col>
-          </Row>
+                          <Form.Group controlId="pasetoToken">
+                              <Form.Control type="text" placeholder="Paseto token" onChange={(e) => setToken(e.target.value)} />
+                          </Form.Group>
+                          <Button variant="primary" type="submit">
+                              Decode
+                          </Button>
+                      </Form>
+                      <div>
+                          <ReactJsonSyntaxHighlighter obj={payload} />
+                      </div>
+                  </Col>
+              </Row>
+          </main>
+          <style jsx global>{`
+              html, body {
+                padding: 0;
+                margin: 0;
+                font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell,
+                                Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+                color: #24292e;
+                background: #f6f8fa;
+              }
+              * {
+                box-sizing: border-box;
+              }
+      `}</style>
       </Container>
   )
 }
